5ee7ec9816ef8c0a310825bd27e4091228562fe0
Priyanka20017 <49614509+Priyanka20017@users.noreply.github.com>
2022-11-10 04:53:59 +0530

    Initial commit

---
+++ mern-stack/backend/seeder/categories.js
@@ -0,0 +1,82 @@
+const categories = [
+    {
+    name: "Computers",
+    description:
+      "",
+    image: "/images/tablets-category.png",
+    attrs: [
+      { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+      { key: "color", value: ["blue", "red", "green", "black"] },
+    ],
+  },
+  {
+    name: "Computers/Laptops",
+    description:
+      "Computers/Laptops - ",
+    image: "/images/tablets-category.png",
+  },
+  {
+    name: "Computers/Laptops/Lenovo",
+    description:
+      "Computers/Laptops/Lenovo - ",
+    image: "/images/tablets-category.png",
+  },
+  {
+    name: "Computers/Laptops/Dell",
+    description:
+      "Computers/Laptops/Dell - ",
+    image: "/images/tablets-category.png",
+  },
+  {
+    name: "Tablets",
+    description:
+      "Tablets - ",
+    image: "/images/tablets-category.png",
+  },
+  {
+    name: "Monitors",
+    description:
+      "Monitors - ",
+    image: "/images/monitors-category.png",
+  },
+  {
+    name: "Games",
+    description:
+      "Games - ",
+    image: "/images/games-category.png",
+  },
+  {
+    name: "Printers",
+    description:
+      "Printers - ",
+    image: "/images/tablets-category.png",
+  },
+  {
+    name: "Software",
+    description:
+      "Software - ",
+    image: "/images/monitors-category.png",
+  },
+  {
+    name: "Cameras & Accessories",
+    description:
+      "Cameras - ",
+    image: "/images/games-category.png",
+  },
+  {
+    name: "Books",
+    description:
+      "Books - ",
+    image: "/images/tablets-category.png",
+    attrs: [{ key: "genre", value: ["movie", "comedy", "thriller"] }],
+  },
+  {
+    name: "Videos",
+    description:
+      "",
+    image: "/images/monitors-category.png",
+    attrs: [{ key: "genre", value: ["movie", "comedy", "thriller"] }]
+  },
+]
+
+module.exports = categories;
